84e49ab6978e528015ff045702b87d0545bc109c
Grace Rarer <gracerarer@gmail.com>
2021-01-20 10:17:57 -0500

    add validation of usernames and passwords
    frontend and backend both now enforce min and max length of passwords
    and usernames, as well as prohibiting some characters (e.g. space)
    from usernames.

--- core/src/util/validation.ts
+++ core/src/util/validation.ts
@@ -4,3 +4,34 @@ export function minYearsAgo(date: Date, years: number): boolean {
   datePlus.setUTCFullYear(datePlus.getUTCFullYear() + years);
   return datePlus.getTime() <= Date.now();
 }
+
+// username format validation: returns a string if the username does not met a requirement
+export function failsUsernameRequirement(s: string): string | undefined {
+  // string.length returns the number of UTF-16 codepoints, which can be different from the number of characters
+  const characterCount = [...s].length;
+  if (characterCount < 3) {
+    return "Username must be at least 3 characters";
+  }
+  if (characterCount > 32) {
+    return "Username must not be longer than 32 characters";
+  }
+  for (const forbidden of ["@", " ", "\u00A0", "#", "/", "\\", "'", '"', "\n", "\t"]) {
+    if (s.includes(forbidden)) {
+      return (`Username contains forbidden character "${forbidden}"`);
+    }
+  }
+  return undefined;
+}
+
+// password format validation: returns a string if the password does not met a requirement
+export function failsPasswordRequirement(s: string): string | undefined {
+  // string.length returns the number of UTF-16 codepoints, which can be different from the number of characters
+  const characterCount = [...s].length;
+  if (characterCount < 6) {
+    return "Password must be at least 6 characters";
+  }
+  if (characterCount > 128) {
+    return "Password must not be longer than 128 characters";
+  }
+  return undefined;
+}
