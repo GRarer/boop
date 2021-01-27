5d295df388da2e1753c790d7874d8abe166823c9
Grace Rarer <gracerarer@gmail.com>
2021-01-27 15:13:48 -0500

    keep session token in local storage
    the frontend session service now stores the session token and user uuid
    in localstorage, so the user can stay logged in even when refreshing the
    page or closing their browser.

--- core/src/datatypes/authentication.ts
+++ core/src/datatypes/authentication.ts
@@ -7,3 +7,15 @@ export type LoginResponse = {
   userUUID: string;
   sessionToken: string; // used to verify identity when making requests to backend
 };
+
+export function isLoginResponse(x: unknown): x is LoginResponse {
+  if (typeof x !== "object" || x === null) {
+    return false;
+  }
+  for (const field of ["userUUID", "sessionToken"]) {
+    if (typeof (x as any)[field] !== "string") {
+      return false;
+    }
+  }
+  return true;
+}
