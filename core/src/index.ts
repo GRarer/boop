a999534e170c22e4a0a1c4d56d2c1925d17bad57
Grace Rarer <gracerarer@gmail.com>
2021-01-30 17:03:58 -0500

    fail ci checks if eslint detects warnings

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,4 +2,4 @@ export * from "./datatypes/account";
 export * from "./datatypes/authentication";
 export * from "./util/dates";
 export * from "./util/validation";
-export * from "./api";
+export * from "./api";;
