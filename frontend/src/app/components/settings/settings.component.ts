325697b9710b581cf0b98f9c4c643f48593760a2
Grace Rarer <gracerarer@gmail.com>
2021-02-06 13:05:10 -0500

    improve ui feedback for change password form
    "confirm password" box now turns red if it doesn't match the password
    
    change password form no longer turns red after submitting

--- frontend/src/app/components/settings/settings.component.ts
+++ frontend/src/app/components/settings/settings.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UpdatePasswordRequest, UserAccountResponse, UpdateAccountRequest, genderValues, Gender } from 'boop-core';
-import { SessionService } from 'src/app/services/session.service';
 import { ApiService } from 'src/app/services/api.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { equalToSiblingValidator } from 'src/app/util/ngUtils';
 
 
 @Component({
@@ -15,14 +15,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SettingsComponent implements OnInit {
 
   constructor(
-    private sessionService: SessionService,
     private router: Router,
     private apiService: ApiService,
     private snackBar: MatSnackBar,
   ) {}
 
   genderOptions: Gender[] = genderValues;
-
 
   updateUserForm: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required]),
@@ -32,14 +30,21 @@ export class SettingsComponent implements OnInit {
     email: new FormControl('', [Validators.required, Validators.email]),
     birthDate: new FormControl('', [Validators.required]),
   });
+
   changePasswordForm: FormGroup = new FormGroup({
     oldPassword: new FormControl('', [Validators.required]),
     newPassword: new FormControl('', [Validators.required]),
-    confirmNewPassword: new FormControl('', [Validators.required])
+    confirmNewPassword: new FormControl('', [Validators.required, equalToSiblingValidator("newPassword")])
   });
+
   isLoading: boolean = true;
 
   ngOnInit(): void {
+    this.refresh();
+  }
+
+  private refresh(): void {
+    this.isLoading = true;
     void this.apiService.getJSON<UserAccountResponse>("http://localhost:3000/account/info", undefined)
       .then((response) => {
         this.updateUserForm.setValue(
@@ -52,9 +57,8 @@ export class SettingsComponent implements OnInit {
             birthDate: response.birthDate,
           }
         );
+        this.updateUserForm.markAsPristine();
         this.isLoading = false;
-        console.log(this.updateUserForm.value);
-        console.log(this.updateUserForm.valid);
       }).catch((error) => {
         console.error(error);
         this.snackBar.open(
@@ -80,17 +84,21 @@ export class SettingsComponent implements OnInit {
       oldPassword: oldPassword,
       newPassword: newPassword
     };
-
+    this.changePasswordForm.reset();
     this.apiService.putJSON("http://localhost:3000/account/password", request).then(() => {
       this.snackBar.open("Password updated", "Dismiss", { duration: 5000 });
-      this.changePasswordForm.reset();
     }).catch((error) => {
       this.apiService.showErrorPopup(error);
     });
   }
 
   updateUserInfo(): void {
+    if (!this.updateUserForm.valid || this.updateUserForm.pristine) {
+      return;
+    }
+
     const value = this.updateUserForm.value;
+    console.log(value);
 
     const request: UpdateAccountRequest = {
       username: value.username,
@@ -102,9 +110,18 @@ export class SettingsComponent implements OnInit {
     };
 
     this.apiService.putJSON("http://localhost:3000/account/edit", request).then(() => {
-      void this.router.navigate(["/home"]);
+      this.snackBar.open("Account Updated.", "Dismiss", { "duration": 5000 });
+      this.refresh();
     }).catch((error) => {
       this.apiService.showErrorPopup(error);
     });
+  }
+
+  // re-checks whether confirm password matches password and updates control's appearance
+  checkConfirmPassword(): void {
+    const confirmNewPassword = this.changePasswordForm.controls.confirmNewPassword;
+    confirmNewPassword.updateValueAndValidity();
+    confirmNewPassword.markAsDirty();
+    confirmNewPassword.markAsTouched();
   }
 }
