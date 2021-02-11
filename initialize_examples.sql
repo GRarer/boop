bbd9122b6c53d79f66085b814c0549409f33f982
Grace Rarer <gracerarer@gmail.com>
2021-02-11 17:14:01 -0500

    add contact methods to testing users

--- initialize_examples.sql
+++ initialize_examples.sql
@@ -7,76 +7,90 @@ do $$
 declare
   -- all of these test accounts use the password "foobar", which matches this hash
   example_password_hash TEXT := '$2b$09$lAAPQpGfr7wG/D9KzFX1pOTrNpgX5X5AOCunbTxfOdcdmyqnKgOPy';
+
+  -- fake UUIDs
+  georgeUUID UUID := '00000001-0000-0000-0000-000000000000';
+  ramonaUUID UUID := '00000002-0000-0000-0000-000000000000';
+  johnUUID UUID := '00000003-0000-0000-0000-000000000000';
+  aliceUUID UUID := '0000000a-0000-0000-0000-000000000000';
+  bobUUID UUID := '0000000b-0000-0000-0000-000000000000';
+  charlieUUID UUID := '0000000c-0000-0000-0000-000000000000';
+  davidUUID UUID := '0000000d-0000-0000-0000-000000000000';
+
 begin
     -- add example users
     -- gpb (George P Burdell)
     INSERT INTO users
       ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000001-0000-0000-0000-000000000000', 'gpb', example_password_hash, 'George P. Burdell',
+      VALUES (georgeUUID, 'gpb', example_password_hash, 'George P. Burdell',
       'George', 'Male', 'gburdell@gatech.edu', '1909-04-01', false);
     -- ramonac (Ramona Cartwright Burdell)
     INSERT INTO users
       ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000002-0000-0000-0000-000000000000', 'ramona', example_password_hash, 'Ramona Cartwright Burdell',
+      VALUES (ramonaUUID, 'ramona', example_password_hash, 'Ramona Cartwright Burdell',
       'Ramona', 'Female', 'rcartwright@agnesscott.edu', '1936-04-01', false);
     -- JRainwater (John Rainwater)
     INSERT INTO users
     ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000003-0000-0000-0000-000000000000', 'JRainwater', example_password_hash, 'John Rainwater',
+      VALUES (johnUUID, 'JRainwater', example_password_hash, 'John Rainwater',
       'Johnny', null, 'jrainwater@washington.edu', '1952-01-01', false);
 
     -- alice, bob, charlie, and david
     INSERT INTO users
     ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000011-0000-0000-0000-000000000000', 'alice', example_password_hash, 'Alice Exampleton',
+      VALUES (aliceUUID, 'alice', example_password_hash, 'Alice Exampleton',
       'Alice', 'Female', 'alice@example.com', '1995-03-05', false);
     INSERT INTO users
     ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000012-0000-0000-0000-000000000000', 'bob', example_password_hash, 'Robert Defacto',
+      VALUES (bobUUID, 'bob', example_password_hash, 'Robert Defacto',
       'Bob', null, 'bob@example.com', '1999-10-20', false);
     INSERT INTO users
     ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000013-0000-0000-0000-000000000000', 'charlie', example_password_hash, 'Charlie McExampleface',
+      VALUES (charlieUUID, 'charlie', example_password_hash, 'Charlie McExampleface',
       'Charlie', 'Nonbinary', 'charlie@example.com', '2000-07-15', false);
     INSERT INTO users
     ("user_uuid", "username", "bcrypt_hash", "full_name",
       "friendly_name", "gender", "email", "birth_date", "is_admin")
-      VALUES ('00000014-0000-0000-0000-000000000000', 'david', example_password_hash, 'David Défaut',
+      VALUES (davidUUID, 'david', example_password_hash, 'David Défaut',
       'Dave', null, 'david@example.com', '1995-03-30', false);
 
 
     -- friend request from John Rainwater to George P Burdell
     insert into friend_requests(from_user, to_user)
-        values('00000003-0000-0000-0000-000000000000', '00000001-0000-0000-0000-000000000000');
+        values(johnUUID, georgeUUID);
+
+    -- friendships
+    INSERT INTO FRIENDS(user_a, user_b) values
+        (georgeUUID, ramonaUUID),
+        (ramonaUUID, georgeUUID),
+        (aliceUUID, bobUUID),
+        (bobUUID, aliceUUID),
+        (aliceUUID, charlieUUID),
+        (charlieUUID, aliceUUID),
+        (charlieUUID, davidUUID),
+        (davidUUID, charlieUUID);
 
 
-    -- George and Ramona Burdell
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000001-0000-0000-0000-000000000000', '00000002-0000-0000-0000-000000000000');
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000002-0000-0000-0000-000000000000', '00000001-0000-0000-0000-000000000000');
-    -- alice and bob
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000011-0000-0000-0000-000000000000', '00000012-0000-0000-0000-000000000000');
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000012-0000-0000-0000-000000000000', '00000011-0000-0000-0000-000000000000');
-    -- alice and charlie
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000011-0000-0000-0000-000000000000', '00000013-0000-0000-0000-000000000000');
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000013-0000-0000-0000-000000000000', '00000011-0000-0000-0000-000000000000');
-    -- charlie and david
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000013-0000-0000-0000-000000000000', '00000014-0000-0000-0000-000000000000');
-    INSERT INTO FRIENDS(user_a, user_b)
-        values('00000014-0000-0000-0000-000000000000', '00000013-0000-0000-0000-000000000000');
 
+
+    -- contact methods
+    INSERT INTO contact_methods (user_uuid, platform, contact_id) VALUES
+    (georgeUUID, 'WhatsApp', '555-555-5555'),
+    (georgeUUID, 'Discord', 'burdell#1234'),
+    (georgeUUID, 'iMessage', '555-555-5555'),
+    (ramonaUUID, 'WhatsApp', '555-555-1234'),
+    (johnUUID, 'Discord', 'rainwater#1337'),
+    (aliceUUID, 'Signal', '123-555-4567'),
+    (aliceUUID, 'Discord', 'exampleton#5678'),
+    (bobUUID, 'Signal', '123-555-0000'),
+    (bobUUID, 'Line', 'defacto1'),
+    (charlieUUID, 'GroupMe', 'charlie2000');
 
    COMMIT;
    RAISE NOTICE 'Finished creating example users';
