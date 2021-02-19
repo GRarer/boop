df60d124a9bbfb6dedbd658942bb2488dfe88559
Grace Rarer <gracerarer@gmail.com>
2021-02-19 18:08:20 -0500

    reduce number of queries to send a notification
    improves efficiency of sending scheduled notifications. we no longer
    need to separately query to get the vapid subscriptions of everyone we
    send a notification to and to get the name/gender of everyone mentioned
    in a notification message. instead, all of that is fetched in the same
    query that selects who to send notifications to.

--- backend/src/queries/reminderQueries.ts
+++ backend/src/queries/reminderQueries.ts
@@ -3,6 +3,8 @@ import { database } from "../services/database";
 import { Pairing } from "../services/reminders/reminders";
 import { scheduleParameters } from "../services/reminders/scheduleTuning";
 import { v4 as uuidV4 } from 'uuid';
+import { Gender } from "boop-core";
+import webpush from "web-push";
 
 // tokens expire after 1 week
 const pushTokenExpirationTime = 7 * 24 * 60 * 60 * 1000;
@@ -56,13 +58,42 @@ export async function selectPairs(): Promise<Pairing[]> {
       from metafriend_options
       -- note that the resulting set of pairs is likely to contain duplicates of the same user
     ),
+    -- merge friend pairs and friend-of-friend pairs
     all_pairs as (
       select * from friend_pairs union all select * from metafriend_pairs
+    ),
+    -- collect user info for user a
+    a_info as (
+      select user_a, user_b, shared,
+      full_name as full_name_a, friendly_name as friendly_name_a, gender as gender_a, vapid_subs as vapid_subs_a
+      from all_pairs join users on user_a = user_uuid
+    ),
+    -- collect user info for user b
+    ab_info as (
+      select user_a, user_b, shared,
+      full_name_a, friendly_name_a, gender_a, vapid_subs_a,
+      full_name as full_name_b, friendly_name as friendly_name_b, gender as gender_b, vapid_subs as vapid_subs_b
+      from a_info join users on user_b = user_uuid
+    ),
+    -- collect user info for mutual friend if applicable
+    abc_info as (
+      select user_a, user_b,
+      full_name_a, friendly_name_a, gender_a, vapid_subs_a,
+      full_name_b, friendly_name_b, gender_b, vapid_subs_b,
+      full_name as mutual_full_name, friendly_name as mutual_friendly_name, gender as mutual_gender
+      from ab_info left join users on shared = user_uuid
     )
-    select * from all_pairs order by random();`;
+    -- return results in a random order
+    select * from abc_info order by random();`;
 
-    // shared is the mutual friend for friend-of-friend pairs, null for friend pairs
-    type PairRow = {user_a: string; user_b: string; shared: string | null;};
+    // mutual friend columns are null for friend pairs
+    type PairRow = {
+      user_a: string; user_b: string;
+      full_name_a: string; friendly_name_a: string; gender_a: Gender; vapid_subs_a: webpush.PushSubscription[];
+      full_name_b: string; friendly_name_b: string; gender_b: Gender; vapid_subs_b: webpush.PushSubscription[];
+      // mutual friend columns are null for friend pairs
+      mutual_full_name: string | null; mutual_friendly_name: string | null; mutual_gender: Gender | null;
+    };
     const rows: PairRow[] = await database.query(query, [
       Date.now() - scheduleParameters.cooldown,
       scheduleParameters.friendProbability,
@@ -76,9 +107,47 @@ export async function selectPairs(): Promise<Pairing[]> {
       if (!usedUsers.has(row.user_a) && !usedUsers.has(row.user_b)) {
         usedUsers.add(row.user_a);
         usedUsers.add(row.user_b);
-        pairings.push(row.shared === null
-          ? { userA: row.user_a, userB: row.user_b, friends: true }
-          : { userA: row.user_a, userB: row.user_b, friends: false, mutualFriend: row.shared });
+
+        pairings.push(row.mutual_full_name === null
+          ? {
+            friends: true,
+            userA: {
+              uuid: row.user_a,
+              fullName: row.full_name_a,
+              friendlyName: row.friendly_name_a,
+              gender: row.gender_a,
+              vapidSubs: row.vapid_subs_a
+            },
+            userB: {
+              uuid: row.user_b,
+              fullName: row.full_name_b,
+              friendlyName: row.friendly_name_b,
+              gender: row.gender_b,
+              vapidSubs: row.vapid_subs_b
+            }
+          }
+          : {
+            friends: false,
+            userA: {
+              uuid: row.user_a,
+              fullName: row.full_name_a,
+              friendlyName: row.friendly_name_a,
+              gender: row.gender_a,
+              vapidSubs: row.vapid_subs_a
+            },
+            userB: {
+              uuid: row.user_b,
+              fullName: row.full_name_b,
+              friendlyName: row.friendly_name_b,
+              gender: row.gender_b,
+              vapidSubs: row.vapid_subs_b
+            },
+            mutualFriend: {
+              fullName: row.mutual_full_name,
+              friendlyName: row.mutual_friendly_name!,
+              gender: row.mutual_gender
+            }
+          });
       }
     }
     // update timestamps on all selected users
@@ -95,7 +164,7 @@ export async function createNotificationTokens(pair: Pairing): Promise<{tokenToA
   await database.query(
     `insert into push_identity_tokens(token, target_user_uuid, expiration_time) values
     ($1, $2, $5), ($3, $4, $5);`,
-    [tokens.tokenToA, pair.userB, tokens.tokenToB, pair.userA, expirationTime]
+    [tokens.tokenToA, pair.userB.uuid, tokens.tokenToB, pair.userA.uuid, expirationTime]
   );
   return tokens;
 }
