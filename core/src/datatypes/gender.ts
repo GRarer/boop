873a9cb2994cdf02793ac8d7d3de9efd0d1e925c
Grace Rarer <gracerarer@gmail.com>
2021-02-21 14:01:26 -0500

    add gramatical gender to pronoun conjugations

--- core/src/datatypes/gender.ts
+++ core/src/datatypes/gender.ts
@@ -13,6 +13,7 @@ type PronounSet = {
   possessiveDeterminer: string; // e.g. "I read *her* comment"
   possessivePronoun: string; // e.g. "This comment is *hers*"
   reflexive: string;
+  grammaticallyPlural: boolean;
 };
 
 export function pronouns(gender: Gender): PronounSet {
@@ -23,21 +24,24 @@ export function pronouns(gender: Gender): PronounSet {
     object: "her",
     possessiveDeterminer: "her",
     possessivePronoun: "hers",
-    reflexive: "herself"
+    reflexive: "herself",
+    grammaticallyPlural: false,
   };
   case "Male": return {
     subject: "he",
     object: "him",
     possessiveDeterminer: "his",
     possessivePronoun: "his",
-    reflexive: "himself"
+    reflexive: "himself",
+    grammaticallyPlural: false,
   };
   case "Nonbinary": return {
     subject: "they",
     object: "them",
     possessiveDeterminer: "their",
     possessivePronoun: "theirs",
-    reflexive: "themself" // singular themself because it refers to a specific person
+    reflexive: "themselves",
+    grammaticallyPlural: true,
   };
   }
 }
