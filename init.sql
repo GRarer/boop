4cc2dfd1e4b038b837055dde1d57eda0af6bec7f
Grace Rarer <gracerarer@gmail.com>
2021-03-03 22:33:00 -0500

    move notification pair selection to sql function
    selecting possible pairs of users to send notifications to is a very
    long sql query. previously we were sending the entire query from the
    backend to the postgres server (more than 3 kilobytes!) every time.
    making this a function should therefore save a lot of unnecessary
    traffic between node and postgres.

--- init.sql
+++ init.sql
@@ -92,3 +92,98 @@ CREATE TABLE contact_methods(
     -- user's username/number/etc for that platform
     contact_id TEXT NOT NULL
 );
+
+
+-- function for selecting pairs of users to send notifications
+CREATE FUNCTION selectPairs(
+    timestamp_cutoff bigint,
+    friend_probability double precision,
+    metafriend_probability double precision
+) RETURNS TABLE (
+        user_a TEXT, user_b TEXT,
+        full_name_a TEXT, friendly_name_a TEXT, gender_a GENDER_IDENTITY, vapid_subs_a JSONB[],
+        full_name_b TEXT, friendly_name_b TEXT, gender_b GENDER_IDENTITY, vapid_subs_b JSONB[],
+        -- mutual friend fields are null for users who are friends
+        mutual_full_name TEXT, mutual_friendly_name TEXT, mutual_gender GENDER_IDENTITY
+) AS $$
+    -- find all the users that can be sent notifications right now
+    WITH enabled_users AS (
+        SELECT user_uuid
+        FROM users
+        WHERE (do_not_disturb IS NOT true) and (last_push_timestamp is null or last_push_timestamp < timestamp_cutoff)
+    ),
+    -- users with restricted profiles cannot match with friends-of-friends
+	    restricted_users AS (
+		    select user_uuid from users where profile_privacy_level = 'restricted'
+	    ),
+    -- find all the friend-of-friend relationships that don't include someone with a restricted profile
+    metafriends as (
+        select f1.user_a as user_a, f1.user_b as shared, f2.user_b as user_b
+        from friends f1 join friends f2 on f1.user_b = f2.user_a
+        where f1.user_a <> f2.user_b and f2.user_b not in (select f3.user_b from friends f3 where f3.user_a = f1.user_a)
+            and f1.user_a not in (select user_uuid from restricted_users)
+            and f2.user_b not in (select user_uuid from restricted_users)
+    ),
+    -- pick a random subset of users to get notifications about friends
+    friends_chosen_users AS (
+        SELECT user_uuid
+        FROM enabled_users
+        WHERE random() < friend_probability
+    ),
+    -- get the array of possible friends that those users could be paired with
+    friend_options AS (
+        SELECT user_a, array_agg(user_b order by random()) as user_bs
+        FROM friends_chosen_users join friends on friends_chosen_users.user_uuid = friends.user_a
+        group by user_a
+    ),
+    -- pick a random subset of users to get notifications about friends-of-friends
+    metafriends_chosen_users AS (
+        SELECT user_uuid
+        FROM enabled_users
+        WHERE random() < metafriend_probability
+    ),
+    -- get the array of possible friends-of-friends those users could be paired with
+    metafriend_options AS (
+        SELECT user_a, array_agg(ARRAY[user_b, shared] order by random()) as user_bs
+        FROM metafriends_chosen_users join metafriends on metafriends_chosen_users.user_uuid = metafriends.user_a
+        group by user_a
+    ),
+    friend_pairs as (
+        -- select one of the possible friends
+        select user_a, user_bs[1] as user_b, NULL::UUID as shared
+        from friend_options
+        -- note that the resulting set of pairs is likely to contain duplicates of the same user
+    ),
+    metafriend_pairs as (
+        select user_a, user_bs[1][1] as user_b, user_bs[1][2] as shared
+        from metafriend_options
+        -- note that the resulting set of pairs is likely to contain duplicates of the same user
+    ),
+    -- merge friend pairs and friend-of-friend pairs
+    all_pairs as (
+        select * from friend_pairs union all select * from metafriend_pairs
+    ),
+    -- collect user info for user a
+    a_info as (
+        select user_a, user_b, shared,
+        full_name as full_name_a, friendly_name as friendly_name_a, gender as gender_a, vapid_subs as vapid_subs_a
+        from all_pairs join users on user_a = user_uuid
+    ),
+    -- collect user info for user b
+    ab_info as (
+        select user_a, user_b, shared,
+        full_name_a, friendly_name_a, gender_a, vapid_subs_a,
+        full_name as full_name_b, friendly_name as friendly_name_b, gender as gender_b, vapid_subs as vapid_subs_b
+        from a_info join users on user_b = user_uuid
+    ),
+    -- collect user info for mutual friend if applicable
+    abc_info as (
+        select user_a, user_b,
+        full_name_a, friendly_name_a, gender_a, vapid_subs_a,
+        full_name_b, friendly_name_b, gender_b, vapid_subs_b,
+        full_name as mutual_full_name, friendly_name as mutual_friendly_name, gender as mutual_gender
+        from ab_info left join users on shared = user_uuid
+    )
+    -- return results in a random order
+    select * from abc_info order by random();
+$$ LANGUAGE SQL
