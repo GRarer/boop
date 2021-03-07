6c69321a6f4be7675bed64c29851663090c83f0e
Grace Rarer <gracerarer@gmail.com>
2021-03-07 17:17:02 -0500

    show basic info on profile

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProfileResponse } from 'boop-core';
+import { Profile, ProfileResponse } from 'boop-core';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styles: [
-  ]
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-  info?: ProfileResponse;
+  loading: boolean = true;
+  denialReason: string | undefined; // reason why page is not visible
+  profile?: Profile; // info to display if page is visible
+
 
   constructor(
     private apiService: ApiService,
@@ -24,13 +26,26 @@ export class ProfileComponent implements OnInit {
   }
 
   async refresh(): Promise<void> {
+    this.loading = true;
+    this.profile = undefined;
+    this.denialReason = undefined;
+
     const username: string = this.route.snapshot.params["username"] ?? "";
-    this.info = await this.apiService.getJSON<ProfileResponse>(
+    const response = await this.apiService.getJSON<ProfileResponse>(
       "http://localhost:3000/profile/user_profile",
       {
         username
       }
     );
-    console.log(this.info);
+    console.log(response);
+    if (response.visible) {
+      this.profile = response.profile;
+      this.denialReason = undefined;
+      this.loading = false;
+    } else {
+      this.profile = undefined;
+      this.denialReason = response.reason;
+      this.loading = false;
+    }
   }
 }
