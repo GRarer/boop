a2c79d22ac0b1deb06686a898bd68e25af3734dd
Grace Rarer <gracerarer@gmail.com>
2021-03-17 18:30:36 -0400

    fix incorrect cooldown time
    notification cooldown time was intended to be 4 hours, but had actually
    been set to 80 hours

--- backend/src/services/reminders/scheduleTuning.ts
+++ backend/src/services/reminders/scheduleTuning.ts
@@ -4,9 +4,9 @@ import { config } from "../../config";
 // average person is selected around once per day, so gets on average between one and two pushes per day
 const standardScheduleParameters = {
   // 4 hours, minimum time between pushes to the same user
-  cooldown: 28800000,
+  cooldown: 4 * 60 * 60 * 1000,
   // 10 minutes, time between iterations of scheduling (milliseconds)
-  interval: 600000,
+  interval: 10 * 60 * 1000,
   // probability of an eligible user being selected for a friend notification in one iteration
   friendProbability: 0.166,
   // probability of an eligible user being selected for a friend-of-friend notification in one iteration
