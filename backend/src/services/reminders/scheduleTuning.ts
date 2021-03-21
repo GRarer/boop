3f47bc00fce258678d62c1f0cc5ad1f06208a089
Grace Rarer <gracerarer@gmail.com>
2021-03-21 14:38:26 -0400

    reduce frequency of reminder notifications
    increases spontaneous notification cooldown time and adjusts
    probabilities to increase time between reminders

--- backend/src/services/reminders/scheduleTuning.ts
+++ backend/src/services/reminders/scheduleTuning.ts
@@ -3,14 +3,18 @@ import { config } from "../../config";
 
 // average person is selected around once per day, so gets on average between one and two pushes per day
 const standardScheduleParameters = {
-  // 4 hours, minimum time between pushes to the same user
-  cooldown: 4 * 60 * 60 * 1000,
+  // 8 hours, minimum time between pushes to the same user
+  cooldown: 8 * 60 * 60 * 1000,
   // 10 minutes, time between iterations of scheduling (milliseconds)
   interval: 10 * 60 * 1000,
+
+  // probabilities were chosen based on a stochastic simulation
+  // aiming for around 1 friend and 0.2 friend-of-friend selections per user per day
+
   // probability of an eligible user being selected for a friend notification in one iteration
-  friendProbability: 0.166,
+  friendProbability: 81 / 8192,
   // probability of an eligible user being selected for a friend-of-friend notification in one iteration
-  metafriendProbability: 0.024,
+  metafriendProbability: 3 / 2048,
 };
 
 // substitute parameters that cause pushes to happen very frequently for testing purposes
