b04a80401c1d80b2f7ee3fd374e90ca6753e434b
mandra-thomas <mandra-thomas@users.noreply.github.com>
2021-03-31 17:53:40 -0400

    Add templates for messages

--- backend/src/services/reminders/reminderNotificationMessages.ts
+++ backend/src/services/reminders/reminderNotificationMessages.ts
@@ -6,7 +6,10 @@ const friendTemplates: ((friend: NotificationIdentity) => string)[] = [
   (friend) => `Say hi to ${friend.friendlyName}`,
   (friend) => `Catch up with ${friend.fullName}`,
   (friend) => `Would you like to chat with ${friend.friendlyName} today?`,
-  (friend) => `How is ${friend.friendlyName} doing today? Check in on ${pronouns(friend.gender).object} and find out!`
+  (friend) => `How is ${friend.friendlyName} doing today? Check in on ${pronouns(friend.gender).object} and find out!`,
+  (friend) => `Don't be a stranger! Check in on ${friend.friendlyName} and see how ${
+    pronouns(friend.gender).subject}'${pronouns(friend.gender).grammaticallyPlural ? 've' : 's'} been.`,
+  (friend) => `Have you gotten a chance to catch up with ${friend.friendlyName} today?`,
 ];
 
 const metafriendTemplates: ((metafriend: NotificationIdentity, mutualFriend: NotificationIdentity) => string)[] = [
@@ -15,6 +18,10 @@ const metafriendTemplates: ((metafriend: NotificationIdentity, mutualFriend: Not
       mutualFriend.friendlyName}. Would you like to chat with ${pronouns(metafriend.gender).object}?`,
   (metafriend, mutualFriend) => `You and ${metafriend.fullName} are both friends with ${
     mutualFriend.friendlyName}. Would you like to say "hi" to ${pronouns(metafriend.gender).object}?`,
+  (metafriend, mutualFriend) => `Have you met ${metafriend.friendlyName}? You are both friends with ${
+    mutualFriend.friendlyName}. Be outgoing! Start a conversation with ${pronouns(metafriend.gender).object}.`,
+  (metafriend, mutualFriend) => `Do you know ${mutualFriend.friendlyName}'s friend, ${
+    metafriend.friendlyName}? Would you like to chat with ${pronouns(metafriend.gender).object} today?`,
 ];
 
 export async function friendNotificationMessage(friend: NotificationIdentity): Promise<string> {
